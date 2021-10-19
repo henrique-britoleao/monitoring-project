d14bb462d1e6a75d489b835cef4ffe12e4eb8882
camillebeguin <camillejbeguin@gmail.com>
2021-10-19 09:56:29 +0200

    update batch model eval metrics

--- src/Monitoring/performance.py
+++ src/Monitoring/performance.py
@@ -5,8 +5,9 @@ logger = logging.getLogger('main_logger')
 
 import pandas as pd
 from sklearn.base import ClassifierMixin
-from sklearn.metrics import f1_score, accuracy_score,confusion_matrix
-from sklearn.metrics import recall_score, precision_score
+from sklearn.metrics import recall_score, precision_score, f1_score, 
+from sklearn.metrics import accuracy_score, roc_auc_score, jaccard_score
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import json
 
@@ -29,6 +30,8 @@ def main_model_evaluation(clf: ClassifierMixin, X_test: pd.DataFrame, y_test: pd
     dict_metrics['recall'] = recall_score(y_test, y_test_pred)
     dict_metrics['precision'] = precision_score(y_test, y_test_pred)
     dict_metrics['confusion_matrix'] = metric_confusion_matrix(y_test, y_test_pred)
+    dict_metrics['auc'] = roc_auc_score(y_test, y_test_pred)
+    dict_metrics['jaccard_score'] = jaccard_score(y_test, y_test_pred)
 
     return dict_metrics
 
@@ -49,11 +52,17 @@ def save_training_performance_metrics(dict_metrics: dict, conf: dict) -> None:
             + conf['selected_dataset'] + "_" + conf['selected_model'] + '.txt', 'w') as outfile:
         json.dump(str(dict_metrics), outfile)
 
-def save_batch_performance_metrics(dict_metrics: dict, batch_id: str, conf: dict) -> None:
-    # TO MODIFY
+def save_batch_performance_metrics(dict_batch_metrics: dict, batch_id: str, conf: dict) -> None:
     with open(conf['paths']['Outputs_path'] + conf['paths']['folder_metrics'] + 'monitoring_metrics_'
-            + conf['selected_dataset'] + "_" + conf['selected_model'] + '.txt', 'w') as outfile:
-        outfile[batch_id]['metrics'].update(
-            'model_performance_metrics': dict_metrics
-        )
-        json.dump(str(outfile), outfile)
+            + conf['selected_dataset'] + "_" + conf['selected_model'] + '.txt', 'w') as metrics_json_path:
+            dict_metrics = json.load(metrics_json_path)
+
+    dict_metrics[batch_id]["metrics"].update(
+        {
+            "model_performance_metrics": dict_batch_metrics
+        }
+    )
+
+    with open(conf['paths']['Outputs_path'] + conf['paths']['folder_metrics'] + 'monitoring_metrics_'
+            + conf['selected_dataset'] + "_" + conf['selected_model'] + '.txt', 'w') as metrics_json_path:
+        json.dump(dict_metrics,metrics_json_path)
