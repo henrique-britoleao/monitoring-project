9a0b8fd5a52191e73880ebc34d4fd8ed7ab243da
camillebeguin <camillejbeguin@gmail.com>
2021-10-20 17:03:22 +0200

    remove conf arg from modeling functions

--- src/Modeling/modeling.py
+++ src/Modeling/modeling.py
@@ -6,14 +6,15 @@ logger = logging.getLogger('main_logger')
 from Modeling.gridsearch import main_GS_from_estimator_and_params
 from Modeling.models import *
 
-def main_modeling_from_name (X_train, y_train, conf):
+import constants as cst
+
+def main_modeling_from_name (X_train, y_train):
     """
     Main modeling function to fit a model on the training set using the best parameters for the selected model
 
     Args:
         X_train (pd.DataFrame): X_train
         y_train (pd.Series): y_train
-        conf (dict): configuration file containing the selected model
         categorical_columns (list): list of categorical columns
         numerical_columns (list): list of numerical columns
 
@@ -27,16 +28,15 @@ def main_modeling_from_name (X_train, y_train, conf):
     dict_function_train_model = {'random_forest': 'train_RFClassifier',
                                 'lightgbm': 'train_lightgbm'}
 
-    selected_model = conf['selected_model']
-    function_get_GS_params = globals()[dict_function_GS_params[selected_model]]
+    function_get_GS_params = globals()[dict_function_GS_params[cst.selected_model]]
     estimator, params_grid = function_get_GS_params()
 
-    logger.info('Beginning of Grid Search using ' + selected_model)
+    logger.info('Beginning of Grid Search using ' + cst.selected_model)
     best_params, best_score = main_GS_from_estimator_and_params(X_train, y_train, estimator, params_grid)
 
-    function_train = globals()[dict_function_train_model[selected_model]]
+    function_train = globals()[dict_function_train_model[cst.selected_model]]
     model = function_train(X_train, y_train, best_params)
-    logger.info('End of Grid Search using ' + selected_model)
+    logger.info('End of Grid Search using ' + cst.selected_model)
     logger.info('Best parameters are :')
     logger.info(best_params)
     logger.info('best score :' + str(best_score))
