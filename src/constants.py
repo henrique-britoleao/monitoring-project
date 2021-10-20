472d8bf378580bb16e20191c5dccb265e2e350f4
camillebeguin <camillejbeguin@gmail.com>
2021-10-20 17:02:49 +0200

    add model training pipeline

--- src/constants.py
+++ src/constants.py
@@ -1,25 +1,52 @@
 import json, os
 
 # load configurations
-PATH_CONF = "../params/conf/conf.json"
+#PATH_CONF = "../params/conf/conf.json"
+PATH_CONF = "params/conf/conf.json"
 conf = json.load(open(PATH_CONF, 'r'))
 
 # load paths
 INPUTS_PATH = conf["paths"]["Inputs_path"]
 OUTPUTS_PATH = conf["paths"]["Outputs_path"]
 PREPROCESSED_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_preprocessed"])
+PREDICTED_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_predicted"]) 
 MODELS_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_models"])
 METRICS_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_metrics"])
 MONITORING_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_monitoring"])
+
+RAW_TRAIN_FILE_NAME = "sample_df.csv"
+RAW_TRAIN_FILE_PATH = os.path.join(INPUTS_PATH, RAW_TRAIN_FILE_NAME)
+
+RAW_BATCH_FILE_NAME = "batch_df.csv"
+RAW_BATCH_FILE_PATH = os.path.join(INPUTS_PATH, RAW_BATCH_FILE_NAME)
+
+PREPROCESSED_TRAIN_FILE_NAME = "sample_df_preprocessed.csv"
+PREPROCESSED_TRAIN_FILE_PATH = os.path.join(PREPROCESSED_PATH, PREPROCESSED_TRAIN_FILE_NAME)
+
+PREDICTED_TRAIN_FILE_NAME = "sample_df_predicted.csv"
+PREDICTED_TRAIN_FILE_PATH = os.path.join(PREPROCESSED_PATH, PREPROCESSED_TRAIN_FILE_NAME)
+
+PREPROCESSED_BATCH_FILE_NAME = "batch_df_preprocessed.csv"
+PREPROCESSED_BATCH_FILE_PATH = os.path.join(PREPROCESSED_PATH, PREPROCESSED_BATCH_FILE_NAME)
+
+PREDICTED_BATCH_FILE_NAME = "batch_df_predicted.csv"
+PREDICTED_BATCH_FILE_PATH = os.path.join(PREDICTED_PATH, PREDICTED_BATCH_FILE_NAME)
+
+TRAINING_METRICS_FILE_NAME = "training_metrics"
+TRAINING_METRICS_FILE_PATH = os.path.join(METRICS_PATH, TRAINING_METRICS_FILE_NAME)
 
 MONITORING_METRICS_FILE_NAME = "metrics.json"
 MONITORING_METRICS_FILE_PATH = os.path.join(MONITORING_PATH, MONITORING_METRICS_FILE_NAME)
 
 
 selected_dataset = conf["selected_dataset"]
+selected_model = conf["selected_model"]
 selected_dataset_information = conf["dict_info_files"][selected_dataset]
 
 y_name = selected_dataset_information["y_name"]
+y_pred = f"{y_name}_pred"
+y_pred_proba = f"{y_name}_pred_proba"
+y_class_labels = selected_dataset_information["y_labels"]
 
 columns_nature = selected_dataset_information["column_nature"]
 categorical_columns = columns_nature["categorical_columns"]
---
+++ src/train_model.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import logging
+logger = logging.getLogger(__name__)
+
+from Loading import loading
+from Preprocessing import preprocessing
+from Modeling import modeling
+from Evaluation import evaluation
+from Utils import utils as u
+
+import constants as cst
+
+def main_model_training_pipeline():
+    sample_df = load_training_data()
+
+    # Preprocess and save training data 
+    print('Preprocess')
+    sample_df_preprocessed = preprocess_training_data(sample_df)
+    save_preprocessed_training_data(sample_df_preprocessed)
+
+    # Train and save model
+    print('Train model')
+    model, _ = modeling.main_modeling_from_name(
+        sample_df_preprocessed.drop(columns=[cst.y_name]),
+        sample_df_preprocessed[cst.y_name]
+        )
+    u.save_model(model)
+
+    # Compute and save model performance metrics
+    print('Get perf metrics')
+    training_performance_metrics = evaluation.cross_evaluate_model_performance(
+        model,
+        sample_df_preprocessed.drop(columns=[cst.y_name]),
+        sample_df_preprocessed[cst.y_name]
+    )
+    u.save_training_performance_metrics(training_performance_metrics)
+
+    # Make predictions on the training set and save results
+    sample_df_preprocessed_pred = make_predictions_on_training_data(model, sample_df_preprocessed)
+    save_predicted_training_data(sample_df_preprocessed_pred)
+
+def load_training_data():
+    logger.info("Load training data")
+    sample_df = loading.read_csv_from_path(cst.RAW_TRAIN_FILE_PATH)
+    return sample_df 
+
+def preprocess_training_data(sample_df):
+    logger.info("Preprocess training data")
+    preprocessor = preprocessing.MarketingPreprocessor()
+    df_preprocessed = preprocessor(sample_df, cst.column_types)
+    return df_preprocessed
+
+def save_preprocessed_training_data(sample_df_preprocessed):
+    loading.write_csv_from_path(sample_df_preprocessed, cst.PREPROCESSED_TRAIN_FILE_PATH)
+
+def make_predictions_on_training_data(model, sample_df_preprocessed):
+    """Add predictions to the preprocessed training dataset
+
+    Args:
+        model (Pipeline): trained model
+        sample_df_preprocessed (pd.DataFrame): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    sample_df_preprocessed_pred = sample_df_preprocessed.copy()
+    sample_df_preprocessed_pred[cst.y_pred] = model.predict(sample_df_preprocessed.drop(cst.y_name, axis=1))
+    sample_df_preprocessed_pred[
+        [f"{cst.y_pred_proba}_{cst.y_class_labels[0]}", 
+        f"{cst.y_pred_proba}_{cst.y_class_labels[1]}"]
+        ] = model.predict_proba(sample_df_preprocessed.drop(cst.y_name, axis=1))
+
+    return sample_df_preprocessed_pred
+
+def save_predicted_training_data(sample_df_preprocessed_pred):
+    """Save training data with predicted labels and corresponding probabilities
+
+    Args:
+        sample_df_preprocessed_pred (pd.DataFrame): preprocessed train data with predictions
+    """
+    loading.write_csv_from_path(sample_df_preprocessed_pred, cst.PREDICTED_TRAIN_FILE_PATH)
