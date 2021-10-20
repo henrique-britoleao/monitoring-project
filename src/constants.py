cb85d84dc0aaa6b17e010e78c5f3a87a16657da8
henrique-britoleao <hbl451@gmail.com>
2021-10-20 18:43:15 +0200

    First version batch pipeline

--- src/constants.py
+++ src/constants.py
@@ -5,9 +5,26 @@ import json, os
 from string import Template
 
 # load configurations
-#PATH_CONF = "../params/conf/conf.json"
-PATH_CONF = "params/conf/conf.json"
+PATH_CONF = "../params/conf/conf.json"
+# PATH_CONF = "params/conf/conf.json"
 conf = json.load(open(PATH_CONF, 'r'))
+
+selected_dataset = conf["selected_dataset"]
+selected_model = conf["selected_model"]
+selected_dataset_information = conf["dict_info_files"][selected_dataset]
+
+y_name = selected_dataset_information["y_name"]
+y_pred = f"{y_name}_pred"
+y_pred_proba = f"{y_name}_pred_proba"
+y_class_labels = selected_dataset_information["y_labels"]
+
+columns_nature = selected_dataset_information["column_nature"]
+categorical_columns = columns_nature["categorical_columns"]
+binary_columns = columns_nature["binary_columns"]
+numerical_columns = columns_nature["numerical_columns"]
+
+column_types = selected_dataset_information["column_types"]
+
 
 # load paths
 INPUTS_PATH = conf["paths"]["Inputs_path"]
@@ -18,15 +35,10 @@ PREDICTED_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_predicted"])
 MODELS_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_models"])
 METRICS_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_metrics"])
 MONITORING_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_monitoring"])
-PREDICTIONS_PATH = os.path.join(OUTPUTS_PATH, conf["paths"]["folder_predictons"])
-
 
 # build file paths
-RAW_TRAIN_FILE_NAME = "sample_df.csv"
+RAW_TRAIN_FILE_NAME = selected_dataset_information['file_name']
 RAW_TRAIN_FILE_PATH = os.path.join(INPUTS_PATH, RAW_TRAIN_FILE_NAME)
-
-RAW_BATCH_FILE_NAME = "batch_df.csv"
-RAW_BATCH_FILE_PATH = os.path.join(INPUTS_PATH, RAW_BATCH_FILE_NAME)
 
 PREPROCESSED_TRAIN_FILE_NAME = "sample_df_preprocessed.csv"
 PREPROCESSED_TRAIN_FILE_PATH = os.path.join(PREPROCESSED_PATH, PREPROCESSED_TRAIN_FILE_NAME)
@@ -46,27 +58,6 @@ TRAINING_METRICS_FILE_PATH = os.path.join(METRICS_PATH, TRAINING_METRICS_FILE_NA
 MONITORING_METRICS_FILE_NAME = "metrics.json"
 MONITORING_METRICS_FILE_PATH = os.path.join(MONITORING_PATH, MONITORING_METRICS_FILE_NAME)
 
-PROBA_PREDICTIONS_FILE_NAME = "batch_proba_predictions.pickle"
-PROBA_PREDICTIONS_FILE_PATH = os.path.join(PREDICTIONS_PATH, PROBA_PREDICTIONS_FILE_NAME)
-
-PREDICTIONS_FILE_NAME = "batch_predictions.pickle"
-PREDICTIONS_FILE_PATH = os.path.join(PREDICTIONS_PATH, PREDICTIONS_FILE_NAME)
-
-
-selected_dataset = conf["selected_dataset"]
-selected_model = conf["selected_model"]
-selected_dataset_information = conf["dict_info_files"][selected_dataset]
-
-y_name = selected_dataset_information["y_name"]
-y_pred = f"{y_name}_pred"
-y_pred_proba = f"{y_name}_pred_proba"
-y_class_labels = selected_dataset_information["y_labels"]
-
-columns_nature = selected_dataset_information["column_nature"]
-categorical_columns = columns_nature["categorical_columns"]
-binary_columns = columns_nature["binary_columns"]
-numerical_columns = columns_nature["numerical_columns"]
-
-column_types = selected_dataset_information["column_types"]
+LOG_FILE_PATH = conf["path_log"]
 
 BATCH_NAME_TEMPLATE = Template('batch${id}.csv')
