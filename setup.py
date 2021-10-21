3326664c2850f542f0c94fd662136d0638112ab5
camillebeguin <camillejbeguin@gmail.com>
2021-10-21 10:46:10 +0200

    add setup.py in branch

---
+++ setup.py
@@ -0,0 +1,42 @@
+#####  Imports  #####
+import logging
+import sys
+import os
+
+#####  Get logger  ######
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
+#####  Set globals  #####
+required_folders = [
+    './Outputs/Metrics',
+    './Outputs/Models',
+    './Outputs/Monitoring',
+    './Outputs/Predicted',
+    './Outputs/Preprocessed',
+    './Inputs/Batches',
+    './params/logs'
+]
+
+required_files = [
+    './Outputs/Monitoring/metrics.json',
+    './Outputs/Monitoring/performance.json'
+]
+
+#####  Set up folder structure  #####
+if __name__=="__main__":
+    logger.info('Started setup')
+
+    for folder in required_folders:
+        try:
+            os.makedirs(folder)
+            logger.info(f'Created folder: {folder}')
+        except FileExistsError:
+            logger.info(f'Already found folder: {folder}')
+            
+
+    for file in required_files:
+        with open(file, mode='a'): 
+            logger.info(f'Ensured file is created: {folder}')
