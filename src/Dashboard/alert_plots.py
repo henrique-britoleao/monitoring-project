ff0e3f25bdbe3b30a3d999592795840b9f014e0c
camillebeguin <camillejbeguin@gmail.com>
2021-10-24 20:46:45 +0200

    add docstrings

--- src/Dashboard/alert_plots.py
+++ src/Dashboard/alert_plots.py
@@ -13,7 +13,15 @@ import constants as cst
 
 
 def alerts_graph(batch_name: str, batch_id: str):
-    '''TODO'''
+    """Function to plot the alert digraph with identified pipeline blocks in red in case of alert 
+
+    Args:
+        batch_name (str): name of the current batch
+        batch_id (str): id of the current batch
+
+    Returns:
+        graphviz object
+    """
     alerts = high_level_alert(batch_name, batch_id)
     alert_drift = max(alerts[2:])
     alerts.insert(3, alert_drift)
@@ -22,17 +30,25 @@ def alerts_graph(batch_name: str, batch_id: str):
             "Binary drift"]
     colors = ["green" if alerts[i]==0 else "red" for i in range(len(alerts))]
     names_emo = [names[i]+" ✔️" if alerts[i]==0 else names[i]+" ❗" for i in range(len(alerts))] 
-    graph = graphviz.Digraph()
-    graph.attr('node', style='filled')
+    fig = graphviz.Digraph()
+    fig.attr('node', style='filled')
     for i, alert in enumerate(alerts):
-        graph.node(str(i+1), names_emo[i], color=colors[i])
-    graph.attr(rankdir='LR')
-    graph.edges(['12', '23', '34', '35', '36', '37'])
-    return graph
+        fig.node(str(i+1), names_emo[i], color=colors[i])
+    fig.attr(rankdir='LR')
+    fig.edges(['12', '23', '34', '35', '36', '37'])
+    return fig
 
 
 def high_level_alert(batch_name: str, batch_id: str) -> list:
-    '''TODO'''
+    """Read metrics in the json monitoring file and identify high alerts.
+
+    Args:
+        batch_name (str): name of the current batch
+        batch_id (str): id of the current batch
+
+    Returns:
+        list: list of metric values 
+    """
     with open(cst.MONITORING_METRICS_FILE_PATH, "r") as read_file:
         dict_metrics = json.load(read_file)
     data_quality = read_alerts.read_data_quality_alerts(dict_metrics, batch_name, batch_id)
@@ -43,7 +59,15 @@ def high_level_alert(batch_name: str, batch_id: str) -> list:
 
 
 def alerts_matrix(batch_name: str, batch_id: str):
-    '''TODO'''
+    """Build an alert matrix from the json monitoring file
+
+    Args:
+        batch_name (str): name of the current batch
+        batch_id (str): id of the current batch
+
+    Returns:
+        fig: plotly heatmap
+    """
     with open(cst.MONITORING_METRICS_FILE_PATH, "r") as read_file:
         dict_metrics = json.load(read_file)
     numerical_drift, categorical_drift, binary_drift = read_alerts.read_covariate_metrics_alerts(dict_metrics, batch_name, batch_id)
--- src/Dashboard/plot_utils.py
+++ src/Dashboard/plot_utils.py
@@ -4,6 +4,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 def update_fig_centered_title(fig, title):
+    """Function to center the figure title
+
+    Args:
+        fig (Figure): plotly figure
+        title (str): title to display
+    """
     fig.update_layout(
         title={
             'text': title,
